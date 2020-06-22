1e25dc8e0edb3e85f2a5518967cf247d47696ca3
kimchanhyung98 <kimchanhyung98@gmail.com>
2020-06-22 20:43:16 +0900

    Create keyword.py

---
+++ functions/keyword.py
@@ -0,0 +1,6 @@
+# keyword.py
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
